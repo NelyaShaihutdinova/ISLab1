2af674976ff3acf4bc979e9d199509c6305ae532
NelyaShaihutdinova <fohado@mail.ru>
2025-09-23 21:05:23 +0300

    init

---
+++ src/test/java/org/example/iblab1/IbLab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.iblab1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbLab1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
